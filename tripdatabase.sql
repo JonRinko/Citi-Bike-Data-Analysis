e0b376199dfad41b1a72f79fa9b8131bdd1f9046
Jon Rinko <jonjr23@gmail.com>
2020-01-17 06:41:07 -0500

    combined postgres tables

--- tripdatabase.sql
+++ tripdatabase.sql
@@ -1,25 +1,35 @@
--- primary key id check (we need it here)
--- csv file name check (we don't need it)
-SELECT * FROM september_2019;
--- copied the table
-CREATE TABLE cumulative AS TABLE september_2019 WITH NO DATA;
-SELECT * FROM cumulative;
+SELECT * FROM cumulative
+Order by starttime desc
+LIMIT 10;
 
--- primary key id add
-ALTER TABLE cumulative
-ADD COLUMN id SERIAL NOT NULL;
+SELECT * FROM feb_2019
+LIMIT 10;
 
+ALTER TABLE cumulative 
+RENAME COLUMN end_station_longitudem TO end_station_longitude;
 
-ALTER TABLE cumulative ADD PRIMARY KEY (id);
+INSERT INTO public.cumulative( tripduration, starttime, stoptime, start_station_id, start_station_name, 
+							  	start_station_latitude, start_station_longitude, end_station_id, end_station_name, 
+							  		end_station_latitude, end_station_longitude, bikeid, usertype, birth_year, gender) 
+										(SELECT * FROM september_2018);
+										
+INSERT INTO public.cumulative( tripduration, starttime, stoptime, start_station_id, start_station_name, 
+							  	start_station_latitude, start_station_longitude, end_station_id, end_station_name, 
+							  		end_station_latitude, end_station_longitude, bikeid, usertype, birth_year, gender) 
+										(SELECT * FROM september_2019);
 
--- modify order of table by re-creating it
-
-SELECT * FROM cumulative;
--- insert data from direct import
-INSERT INTO public.cumulative(
-	tripduration, starttime, stoptime, start_station_id, start_station_name, start_station_latitude, start_station_longitude, end_station_id, end_station_name, end_station_latitude, end_station_longitudem, bikeid, usertype, birth_year, gender)
-	 (SELECT * FROM april_2018);
-
-INSERT INTO public.cumulative(
-	tripduration, starttime, stoptime, start_station_id, start_station_name, start_station_latitude, start_station_longitude, end_station_id, end_station_name, end_station_latitude, end_station_longitudem, bikeid, usertype, birth_year, gender)
-	 (SELECT * FROM april_2018);
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
+							
